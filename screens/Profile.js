74531a7a78648038a4201d3758683e466c029867
ameerwattad <ameerwattad20@gmail.com>
2024-05-23 23:28:50 +0300

    small changes

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity, Image, Modal, Pressable } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
-import { Card } from 'react-native-paper';
-import * as ImagePicker from 'expo-image-picker'; // Import from expo-image-picker
+import { Card, Menu, Provider } from 'react-native-paper';
+import * as ImagePicker from 'expo-image-picker';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDrSb4tLwIq0Oh_7bsKst95Po3n20-i10Q",
@@ -20,8 +20,8 @@ const auth = getAuth();
 
 export default function Profile({ navigation }) {
   const [user, setUser] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
   const [image, setImage] = useState(null);
+  const [menuVisible, setMenuVisible] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -32,14 +32,12 @@ export default function Profile({ navigation }) {
   }, []);
 
   const pickImage = async () => {
-    // Request permission to access the image library
     const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (permissionResult.granted === false) {
       alert("Permission to access the camera roll is required!");
       return;
     }
 
-    // Launch the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -49,19 +47,17 @@ export default function Profile({ navigation }) {
 
     if (!result.cancelled) {
       setImage(result.assets[0].uri);
-      setModalVisible(false);
+      setMenuVisible(false);
     }
   };
 
   const takePicture = async () => {
-    // Request permission to access the camera
     const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
     if (permissionResult.granted === false) {
       alert("Permission to access the camera is required!");
       return;
     }
 
-    // Launch the camera
     let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -71,7 +67,7 @@ export default function Profile({ navigation }) {
 
     if (!result.cancelled) {
       setImage(result.assets[0].uri);
-      setModalVisible(false);
+      setMenuVisible(false);
     }
   };
 
@@ -86,57 +82,45 @@ export default function Profile({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-      {user ? (
-        <>
-          <View style={styles.profileIcon}>
-            <TouchableOpacity onPress={() => setModalVisible(true)}>
-              {image ? (
-                <Image source={{ uri: image }} style={styles.profileImage} />
-              ) : (
-                <Image source={require('../assets/images/Profile.png')} style={styles.profileImage} />
-              )}
-            </TouchableOpacity>
-          </View>
-          <View style={styles.signout}>
-            <TouchableOpacity onPress={handleLogout}>
-              <Card>
-                <Card.Content style={styles.signoutcontent}>
-                  <Text style={styles.signoutTextcontent}>Sign out</Text>
-                </Card.Content>
-              </Card>
-            </TouchableOpacity>
-          </View>
-        </>
-      ) : (
-        <Text>No user is logged in</Text>
-      )}
-
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(!modalVisible)}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Pressable style={[styles.modalButton, styles.takePhotoButton]} onPress={takePicture}>
-              <Text style={styles.modalButtonText}>Take a Photo</Text>
-            </Pressable>
-            <Pressable style={[styles.modalButton, styles.choosePhotoButton]} onPress={pickImage}>
-              <Text style={styles.modalButtonText}>Choose a photo from Library</Text>
-            </Pressable>
-            <Pressable
-              style={[styles.modalButton, styles.closeButton]}
-              onPress={() => setModalVisible(false)}
-            >
-              <Text style={styles.modalButtonText}>Cancel</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text style={styles.title}>Profile</Text>
+        {user ? (
+          <>
+            <View style={styles.profileIcon}>
+              <Menu
+                visible={menuVisible}
+                onDismiss={() => setMenuVisible(false)}
+                anchor={
+                  <TouchableOpacity onPress={() => setMenuVisible(true)}>
+                    {image ? (
+                      <Image source={{ uri: image }} style={styles.profileImage} />
+                    ) : (
+                      <Image source={require('../assets/images/Profile.png')} style={styles.profileImage} />
+                    )}
+                  </TouchableOpacity>
+                }
+                contentStyle={styles.menu}
+              >
+                <Menu.Item onPress={takePicture} title="Take a Photo" titleStyle={styles.menuItem} />
+                <Menu.Item onPress={pickImage} title="Choose from Library" titleStyle={styles.menuItem} />
+              </Menu>
+            </View>
+            <View style={styles.signout}>
+              <TouchableOpacity onPress={handleLogout}>
+                <Card>
+                  <Card.Content style={styles.signoutcontent}>
+                    <Text style={styles.signoutTextcontent}>Sign out</Text>
+                  </Card.Content>
+                </Card>
+              </TouchableOpacity>
+            </View>
+          </>
+        ) : (
+          <Text>No user is logged in</Text>
+        )}
+      </View>
+    </Provider>
   );
 }
 
@@ -175,34 +159,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    padding: 20,
+  menu: {
+    backgroundColor: '#f5f5f5',
     borderRadius: 10,
   },
-  modalButton: {
-    padding: 15,
-    marginBottom: 10,
-    borderRadius: 5,
-    backgroundColor: '#add8e6',
-  },
-  modalButtonText: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  takePhotoButton: {
-    backgroundColor: '#add8e6',
-  },
-  choosePhotoButton: {
-    backgroundColor: '#add8e6',
-  },
-  closeButton: {
-    backgroundColor: '#ff6347', // Red color for cancel button
+  menuItem: {
+    fontSize: 16,
+    color: '#333',
   },
 });
